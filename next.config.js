96982f505905f8fb657c8fa918d1df17d4544ec5
梁凯俊 <8329454+liang-kaijun@user.noreply.gitee.com>
2025-03-25 17:14:29 +0800

    [feature]官网最新版本代码

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/home', 
+  images: {
+    unoptimized: true,  // 设置为 true 可以避免图片优化服务的问题
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.ai2049.com',
+      },
+    ],
+  }, 
+};
+
+module.exports = nextConfig; 
